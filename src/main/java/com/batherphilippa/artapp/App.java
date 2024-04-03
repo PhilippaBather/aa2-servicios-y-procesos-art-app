b588e36c93c21ebfa37774ab9b66bdd8bb8202e8
pabather <p.a.bather1@gmail.com>
2024-04-03 10:25:16 +0200

    added main class

---
+++ src/main/java/com/batherphilippa/artapp/App.java
@@ -0,0 +1,11 @@
+package com.batherphilippa.artapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}

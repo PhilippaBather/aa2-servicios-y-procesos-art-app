2191705f7af385b4ce1c590a63ef707798ad0afc
pabather <p.a.bather1@gmail.com>
2024-04-03 11:09:54 +0200

    class created to define UI output

--- src/main/java/com/batherphilippa/artapp/model/ArtworkOutput.java
+++ src/main/java/com/batherphilippa/artapp/model/ArtworkOutput.java
@@ -1,2 +1,13 @@
-package com.batherphilippa.artapp.model;public class ArtworkOutput {
+package com.batherphilippa.artapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArtworkOutput {
+    private long id;
+    private String title;
 }
